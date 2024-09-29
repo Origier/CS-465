118e2da85982386db7c368e1cf5c64e09c9784b4
Aaron McDonald <sergeantbeast456@gmail.com>
2024-09-29 12:33:12 -0400

    feat: module 5 starter

---
+++ app_api/routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express'); // Express app
+const router = express.Router();    // Router logic
+
+// This is where we import the controllers we will route
+const tripsController = require('../controllers/trips');
+
+console.log("Registering router");
+
+// define route for our trips endpoint
+router
+	.route('trips')
+	.get(tripsController.tripsList); // GET method routes tripList
+
+module.exports = router;
