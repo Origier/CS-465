fc89c46e1336c28fc49a92c355fa9edfa13615ec
Aaron <sergeantbeast456@gmail.com>
2024-09-29 14:01:08 -0400

    feat: Compeleted Module Five Baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -2,12 +2,9 @@ const mongoose = require('mongoose');
 const Trip = require('../models/travlr'); // Register Model
 const Model = mongoose.model('trips');
 
-console.log("Fetching trips");
-
 // GET : /trips - lists all the trips
 // Regardless of outcome, response must include HTML status code
 // and JSON message to the requesting client
-
 const tripsList = async(req, res) => {
 	const q = await Model
 		.find({}) // No filter, return all records
@@ -15,8 +12,32 @@ const tripsList = async(req, res) => {
 
 		// Uncomment the following line to show results of querey
 		// on the console
-		console.log("Here is what was found");
-		console.log(q);
+		// console.log(q);
+	
+	if(!q) {
+		// Database returned no data
+		return res
+			.status(404)
+			.json(err);
+	} else {
+		// Return resulting trip list
+		return res
+			.status(200)
+			.json(q);
+	}
+};
+
+// GET: /trips/:tripCode - lists a single trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsFindByCode = async(req, res) => {
+	const q = await Model
+		.find({'code' : req.params.tripCode}) // Return a single record
+		.exec();
+
+		// Uncomment the following line to show results of querey
+		// on the console
+		// console.log(q);
 	
 	if(!q) {
 		// Database returned no data
@@ -32,5 +53,6 @@ const tripsList = async(req, res) => {
 };
 
 module.exports = {
-	tripsList
+	tripsList,
+    tripsFindByCode
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -4,11 +4,14 @@ const router = express.Router();    // Router logic
 // This is where we import the controllers we will route
 const tripsController = require('../controllers/trips');
 
-console.log("Registering router");
-
 // define route for our trips endpoint
 router
-	.route('trips')
-	.get(tripsController.tripsList); // GET method routes tripList
+    .route('/trips')
+    .get(tripsController.tripsList); // GET method routes tripList
+
+// GET Method routes tripsFindByCode - requires parameter
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindByCode)
 
 module.exports = router;
